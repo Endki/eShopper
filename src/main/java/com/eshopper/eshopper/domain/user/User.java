882ec086274b96d505c7e9a20f42d331dd4b6a43
KSJ <khb3221@naver.com>
2022-02-10 21:25:30 +0900

    #feat 유저 정보 클래스화, 레포지토리화

---
+++ src/main/java/com/eshopper/eshopper/domain/user/User.java
@@ -0,0 +1,67 @@
+package com.eshopper.eshopper.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column
+    private String pcode;
+    @Column
+    private String loadAddr;
+    @Column
+    private String lotAddr;
+
+    @Column
+    private String extraAddr;
+    @Column
+    private String etcAddr;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name,String email, String picture, Role role){
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture){
+        this.name=name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+}
